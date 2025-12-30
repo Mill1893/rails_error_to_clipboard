84baa10b95d7bef3d8ae73ba4a5b704bd05dc13a
Mill1893 <andrew.miller14.am@gmail.com>
2025-12-29 18:58:18 -0600

    fix: add startup debugging to verify middleware registration

--- lib/rails_error_to_clipboard/railtie.rb
+++ lib/rails_error_to_clipboard/railtie.rb
@@ -2,15 +2,16 @@
 
 module RailsErrorToClipboard
   class Railtie < Rails::Railtie
-    console do
-      puts '[rails_error_to_clipboard] Gem loaded successfully'
-    end
-
     config.before_configuration do
       RailsErrorToClipboard.configure {}
-      Rails.logger&.info '[rails_error_to_clipboard] Railtie initialized'
+      puts '[rails_error_to_clipboard] Railtie initializing...'
     end
 
-    config.app_middleware.insert_before(ActionDispatch::ShowExceptions, RailsErrorToClipboard::Middleware)
+    config.after_initialize do
+      middleware_names = Rails.configuration.app_middleware.map(&:name).map(&:to_s)
+      puts "[rails_error_to_clipboard] Middleware in stack: #{middleware_names.include?('RailsErrorToClipboard::Middleware')}"
+    end
+
+    config.app_middleware.insert_after(ActionDispatch::ShowExceptions, RailsErrorToClipboard::Middleware)
   end
 end
